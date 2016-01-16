bc3d98f7db6197a2e0a6c04c89edcb69026fc21b
Alexander Pronin <pronin.alx@gmail.com>
2016-01-16 16:54:27 +0200

    Final commit

--- WallpaperDemo/WallpaperDemo/ViewController.swift
+++ WallpaperDemo/WallpaperDemo/ViewController.swift
@@ -9,40 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  @IBOutlet weak var currentMonth: UILabel!
-  @IBOutlet weak var imageView: UIImageView!
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  @IBAction func sliderUpdated(sender: AnyObject) {
-    let slider = sender as! UISlider
-    currentMonth.text = "\(Int(slider.value))"
-  }
-  
-  @IBAction func close(segue:UIStoryboardSegue) {
     
-  }
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     
-    @IBAction func goTapped(sender:AnyObject) {
-      performSegueWithIdentifier("GoToWallpaper", sender: sender)
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-  
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close(segue:UIStoryboardSegue) {
+        
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-      if segue.identifier == "GoToWallpaper" {
-        let paperViewController = segue.destinationViewController as! PaperViewController
-        paperViewController.monthToShow = Int(currentMonth.text!)!
-      }
+        if segue.identifier == "GoToWallpaper" {
+            let paperViewController = segue.destinationViewController as! PaperViewController
+            if let indexPath = tableView.indexPathForSelectedRow {
+                paperViewController.monthToShow = indexPath.row + 1
+            }
+        }
     }
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return months.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("WallpaperCell", forIndexPath: indexPath)
+        cell.textLabel?.text = months[indexPath.row]
+        cell.imageView?.image = UIImage(named: String(format: "%02d.jpg", arguments: [indexPath.row + 1]))
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("GoToWallpaper", sender: nil)
+    }
+}
